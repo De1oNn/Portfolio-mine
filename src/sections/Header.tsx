5d8dd3176d2c8fd5e1ec4814c97e4bca6d07737b
24LP2782 24LP2782 <24LP2782@G301-CC0200.local>
2025-04-26 15:08:29 +0800

    commit

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,11 @@
 export const Header = () => {
     return (
-        <div className="flex justify-between items-center relative top-3">
-            <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full">
-                <a href="#" className="nav-item">Home</a>
-                <a href="#" className="nav-item">Projects</a>
-                <a href="#" className="nav-item">About</a>
-                <a href="#" className="nav-item">Contact</a>
+        <div className="flex justify-between items-center fixed top-3">
+            <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+                <a href="#" className="nav-item hover:bg-white/10 ">Home</a>
+                <a href="#" className="nav-item hover:bg-white/10">Projects</a>
+                <a href="#" className="nav-item hover:bg-white/10">About</a>
+                <a href="#" className="nav-item bg-white text-gray-900 hover:bg-white/10 hover:text-white">Contact</a>
             </nav>
         </div>
     )
